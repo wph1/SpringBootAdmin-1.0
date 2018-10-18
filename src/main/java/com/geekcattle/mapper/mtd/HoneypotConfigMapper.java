eb84253ca57e871bbc075b6927c43283395ad5ce
wangpenghao <wangpenghao@126.com>
2018-10-18 18:10:48 +0800

    mtd功能初始化

---
+++ src/main/java/com/geekcattle/mapper/mtd/HoneypotConfigMapper.java
@@ -0,0 +1,9 @@
+package com.geekcattle.mapper.mtd;
+
+import com.geekcattle.model.mtd.HoneypotConfig;
+import com.geekcattle.util.CustomerMapper;
+import org.springframework.stereotype.Component;
+
+@Component
+public interface HoneypotConfigMapper extends CustomerMapper<HoneypotConfig> {
+}
